5808472eb436af6bf0ccb010f01ef954c4eb4e1a
Maulida <isnariskimaulida727@gmail.com>
2025-11-02 09:47:46 +0700

    url base update

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/si25_webgis_isna_2/public/';
+    public string $baseURL = 'http://localhost/si25_webgis_isna2/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
